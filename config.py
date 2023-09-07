1f4797461f135d089574df5c306f6f89089bb2db
AlpacaMoon <66011139+AlpacaMoon@users.noreply.github.com>
2023-09-07 16:32:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c1hwd8ybo78z.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "thongsoxue-employee"
+customregion = "us-east-1"
+
